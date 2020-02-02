75e1fa0a42a64829f893fbbe98fea524a0e5207e
Pjeferson <paulojeferson34@gmail.com>
2020-02-02 20:59:30 -0300

    Adding Router configuration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import '~/config/ReactotronConfig';
 
-import { store, persistor } from './store';
+import history from '~/services/history';
+import { store, persistor } from '~/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <PersistGate persistor={persistor}></PersistGate>
+      <PersistGate persistor={persistor}>
+        <Router history={history} />
+      </PersistGate>
     </Provider>
   );
 }
