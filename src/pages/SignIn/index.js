6baf633b8bb82fd1d469d4aa58b6247ad84d8200
Pjeferson <paulojeferson34@gmail.com>
2020-02-04 10:34:46 -0300

    Creating Sign in page structure

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+      <form action="">
+        <input type="email" name="email" placeholder="Digite seu e-mail" />
+        <input type="password" name="password" placeholder="Sua senha" />
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </form>
+    </>
+  );
 }
