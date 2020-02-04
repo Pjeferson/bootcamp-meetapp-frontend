3918ad25d08c81b279405e67fcce9fd8a88df660
Pjeferson <paulojeferson34@gmail.com>
2020-02-04 10:35:01 -0300

    Creating sign up page structure

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="MeetApp" />
+      <form action="">
+        <input type="text" name="name" placeholder="Digite seu nome" />
+        <input type="email" name="email" placeholder="Seu e-mail" />
+        <input type="password" name="password" placeholder="Sua senha" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
