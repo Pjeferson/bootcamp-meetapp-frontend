58953583ad455506b0cf23a557131718283bc714
Pjeferson <paulojeferson34@gmail.com>
2020-02-05 15:12:07 -0300

    Adding auth forms validation

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <>
       <img src={logo} alt="MeetApp" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Digite seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Entrar</button>
