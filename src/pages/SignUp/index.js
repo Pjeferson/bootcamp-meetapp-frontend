20937bc9240bf4e75718fd4f79cab95525052958
Pjeferson <paulojeferson34@gmail.com>
2020-02-05 14:49:07 -0300

    Recreating forms with Unform lib

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="MeetApp" />
-      <form action="">
-        <input type="text" name="name" placeholder="Digite seu nome" />
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input type="password" name="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Digite seu nome" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
