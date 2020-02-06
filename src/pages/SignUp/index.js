e6229ea5bafc66a2e602196f960e78fab9bdbe44
Pjeferson <paulojeferson34@gmail.com>
2020-02-06 11:24:56 -0300

    Using sign up action on SignUp page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
@@ -16,15 +19,19 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
-  function handleSubmit(data) {}
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <>
       <img src={logo} alt="MeetApp" />
       <Form schema={schema} onSubmit={handleSubmit}>
-        <Input type="text" name="name" placeholder="Digite seu nome" />
-        <Input type="email" name="email" placeholder="Seu e-mail" />
-        <Input type="password" name="password" placeholder="Sua senha" />
+        <Input name="name" type="text" placeholder="Digite seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
       </Form>
