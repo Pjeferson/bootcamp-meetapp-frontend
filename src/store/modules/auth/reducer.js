b9838e9cd1692b84825b308d744b91b9e8623ed0
Pjeferson <paulojeferson34@gmail.com>
2020-02-06 11:09:20 -0300

    Implementing auth reducer

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,29 @@
+import produce from 'immer';
+
+const INITIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
+export default function auth(state = INITIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'SIGN_IN_REQUEST':
+        draft.loading = true;
+        break;
+      case 'SIGN_IN_SUCCESS':
+        const { token } = action.payload;
+        draft.loading = false;
+        draft.signed = true;
+        draft.token = token;
+        break;
+
+      case 'SIGN_FAILURE':
+        draft.loading = false;
+        break;
+      default:
+        break;
+    }
+  });
+}
