6df5b818d22a37fc1c06453c8316378e532c3023
Pjeferson <paulojeferson34@gmail.com>
2020-02-06 22:31:10 -0300

    Creating default layout structure

---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  background: linear-gradient(180deg, #22202c 0%, #402845 100%);
+  height: 100%;
+`;
