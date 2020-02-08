d2c6e1e7f8f1ab48fe85201e95d0fe9624e534f3
Pjeferson <paulojeferson34@gmail.com>
2020-02-08 11:48:51 -0300

    Styling Dashboard page

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 940px;
+  margin: auto;
+  padding: 50px 0;
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    h1 {
+      font-size: 32px;
+      color: #fff;
+    }
+
+    button {
+      min-height: 42px;
+      min-width: 172px;
+      background: #f94d6a;
+      border: 0;
+      border-radius: 4px;
+      padding: 10px 20px;
+
+      color: #fff;
+      font-size: 16px;
+      font-weight: bold;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        margin-right: 10px;
+      }
+    }
+  }
+
+  ul {
+    margin-top: 50px;
+  }
+`;
+
+export const Appointment = styled.li`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 20px 20px 20px 30px;
+
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  div {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    strong {
+      font-size: 16px;
+      color: #fff;
+    }
+
+    span {
+      font-size: 16px;
+      color: #fff;
+      opacity: 0.6;
+    }
+  }
+
+  button {
+    background: none;
+    border: 0;
+
+    height: 24px;
+    width: 24px;
+    margin-left: 30px;
+  }
+
+  li + & {
+    margin-top: 10px;
+  }
+`;
