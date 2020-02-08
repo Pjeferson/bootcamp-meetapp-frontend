a5f94447a0f91db4c8ffa27aeadeacd2a63346e6
Pjeferson <paulojeferson34@gmail.com>
2020-02-07 23:43:53 -0300

    Creating Header component structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+import { Container, Content, Profile } from './styles';
 
 export default function Header() {
-  return <div />;
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="MeetApp" />
+        <aside>
+          <Profile>
+            <strong>Seu Nome</strong>
+            <Link to="/profile">Meu perfil</Link>
+          </Profile>
+          <button type="button">Sair</button>
+        </aside>
+      </Content>
+    </Container>
+  );
 }
