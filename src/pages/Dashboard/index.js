0ba79570939ce380ecddaeca7e4106843e91fcde
Pjeferson <paulojeferson34@gmail.com>
2020-02-08 11:48:29 -0300

    Getting meetups data from api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
-// import { Container } from './styles';
+import { Container, Appointment } from './styles';
+
+import api from '~/services/api';
 
 export default function Dashboard() {
-  return <div />;
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    async function loadMeetups() {
+      const response = await api.get('/organizing');
+      const data = response.data.map(meetup => ({
+        ...meetup,
+        dateFormatted: format(
+          parseISO(meetup.date),
+          "d 'de' MMMM', às' HH'h'",
+          {
+            locale: pt,
+          }
+        ),
+      }));
+      setMeetups(data);
+    }
+    loadMeetups();
+  }, []);
+  return (
+    <Container>
+      <header>
+        <h1>Meus meetups</h1>
+        <button type="button">
+          <MdAddCircleOutline color="#fff" size={20} />
+          Novo meetup
+        </button>
+      </header>
+
+      <ul>
+        {meetups.map(meetup => (
+          <Appointment key={meetup.id}>
+            <div>
+              <strong>{meetup.title}</strong>
+              <span>{meetup.dateFormatted}</span>
+            </div>
+            <button type="button">
+              <MdChevronRight size={24} color="#fff" />
+            </button>
+          </Appointment>
+        ))}
+      </ul>
+    </Container>
+  );
 }
