de54c34560f8c97a03a29a9a1c3699e1468c207c
Pjeferson <paulojeferson34@gmail.com>
2020-02-08 09:01:31 -0300

    Using user state on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
   return (
     <Container>
       <Content>
         <img src={logo} alt="MeetApp" />
         <aside>
           <Profile>
-            <strong>Seu Nome</strong>
+            <strong>{profile.name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </Profile>
           <button type="button">Sair</button>
