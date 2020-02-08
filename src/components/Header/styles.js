30f25b2a7b2ce3799ac99713248753a96b6ce104
Pjeferson <paulojeferson34@gmail.com>
2020-02-07 23:44:11 -0300

    Styling Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 64px;
+  background: #000;
+  padding: 0 30px;
+`;
+
+export const Content = styled.div`
+  height: 100%;
+  max-width: 940px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  img {
+    height: 32px;
+    width: 32px;
+  }
+  aside {
+    display: flex;
+    align-items: center;
+
+    button {
+      height: 35px;
+      width: 70px;
+      background: #d44059;
+      border: none;
+      border-radius: 4px;
+
+      color: #fff;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  justify-content: space-between;
+
+  margin-right: 30px;
+
+  strong {
+    color: #fff;
+  }
+
+  a {
+    margin-top: 5px;
+    color: #999;
+  }
+`;
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   background: linear-gradient(180deg, #22202c 0%, #402845 100%);
