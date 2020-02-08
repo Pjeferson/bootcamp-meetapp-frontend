0987731ba96086d52ef26fdc6d6d68e1a3998842
Pjeferson <paulojeferson34@gmail.com>
2020-02-08 11:51:00 -0300

    Reajusting views height

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -4,6 +4,7 @@ import { darken } from 'polished';
 export const Wrapper = styled.div`
   background: linear-gradient(180deg, #22202c 0%, #402845 100%);
   height: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
