806b196450e1fb91021bdd0b5130f94ec4f55140
Pjeferson <paulojeferson34@gmail.com>
2020-02-09 19:52:04 -0300

    Adding unform  and refactoring style

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import {MdAddCircleOutline} from 'react-icons/md';
+import { useSelector } from 'react-redux';
+import { MdAddCircleOutline } from 'react-icons/md';
+import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <input name="name" type="text" placeholder="Seu nome"/>
-      <input name="email" type="email" placeholder="Seu e-mail"/>
-      <hr />
-      <input name="oldPassword" type="password" placeholder="Senha atual" />
-      <input name="password" type="password" placeholder="Nova senha"/>
-      <input name="confirmPassword" type="password" placeholder="Confirmação de senha"/>
-      <button type="submit">
-        <MdAddCircleOutline size={20} color="#fff"/>
-        Salvar perfil
-      </button>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <hr />
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">
+          <MdAddCircleOutline size={20} color="#fff" />
+          Salvar perfil
+        </button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -5,54 +5,57 @@ export const Container = styled.div`
   max-width: 940px;
   margin: 50px auto;
 
-  display: flex;
-  flex-direction: column;
-
-  hr {
-    margin: 10px 0 20px;
-    border: 0;
-    height: 1px;
-    background: rgba(255, 255, 255, 0.2);
-  }
-
-  input {
-    height: 50px;
-    border: 0;
-    margin-bottom: 10px;
-    padding: 15px 20px;
-
-    background: rgba(0, 0, 0, 0.2);
-    font-size: 18px;
-    color: #fff;
-
-    &::placeholder {
-      color: rgba(255, 255, 255, 0.5);
-    }
-  }
-
-  button {
-    align-self: flex-end;
+  form {
     display: flex;
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
 
-    height: 42px;
-    width: 162px;
-    padding: 10 20px;
-    background: #f94d6a;
-    border: 0;
-    border-radius: 4px;
-
-    color: #fff;
-    font-size: 16px;
-    font-weight: bold;
-
-    &:hover {
-      background: ${darken(0.03, '#F94D6A')};
+    hr {
+      margin: 10px 0 20px;
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
     }
 
-    svg {
-      margin-right: 10px;
+    input {
+      height: 50px;
+      border: 0;
+      margin-bottom: 10px;
+      padding: 15px 20px;
+
+      background: rgba(0, 0, 0, 0.2);
+      font-size: 18px;
+      color: #fff;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.5);
+      }
+    }
+
+    button {
+      align-self: flex-end;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      margin-top: 10px;
+      height: 42px;
+      width: 162px;
+      padding: 10px;
+      background: #f94d6a;
+      border: 0;
+      border-radius: 4px;
+
+      color: #fff;
+      font-size: 16px;
+      font-weight: bold;
+
+      &:hover {
+        background: ${darken(0.03, '#F94D6A')};
+      }
+
+      svg {
+        margin-right: 10px;
+      }
     }
   }
 `;
