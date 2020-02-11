04580d8218c3c3c1a47ef84b20bf79dff461b07e
Pjeferson <paulojeferson34@gmail.com>
2020-02-11 17:48:38 -0300

    Creating actions and sagas to meetup creation

--- src/store/modules/meetup/actions.js
+++ src/store/modules/meetup/actions.js
@@ -16,3 +16,23 @@ export function loadMeetupsFailure() {
     type: '@meetup/LOAD_MEETUPS_FAILURE',
   };
 }
+
+export function createMeetupRequest(data) {
+  return {
+    type: '@meetup/CREATE_MEETUP_REQUEST',
+    payload: { data },
+  };
+}
+
+export function createMeetupSuccess(meetup) {
+  return {
+    type: '@meetup/CREATE_MEETUP_SUCCESS',
+    payload: { meetup },
+  };
+}
+
+export function createMeetupFailure() {
+  return {
+    type: '@meetup/CREATE_MEETUP_FAILURE',
+  };
+}
--- src/store/modules/meetup/sagas.js
+++ src/store/modules/meetup/sagas.js
@@ -3,8 +3,14 @@ import { format, parseISO } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import api from '~/services/api';
+import history from '~/services/history';
 
-import { loadMeetupsSuccess, loadMeetupsFailure } from './actions';
+import {
+  loadMeetupsSuccess,
+  loadMeetupsFailure,
+  createMeetupSuccess,
+  createMeetupFailure,
+} from './actions';
 
 export function* loadMeetups() {
   try {
@@ -23,4 +29,25 @@ export function* loadMeetups() {
   }
 }
 
-export default all([takeLatest('@meetup/LOAD_MEETUPS_REQUEST', loadMeetups)]);
+export function* createMeetup({ payload }) {
+  try {
+    const { file_id, title, description, date, location } = payload.data;
+    const response = yield call(api.post, '/meetups', {
+      file_id,
+      title,
+      description,
+      date,
+      location,
+    });
+
+    yield put(createMeetupSuccess(response));
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(createMeetupFailure());
+  }
+}
+
+export default all([
+  takeLatest('@meetup/LOAD_MEETUPS_REQUEST', loadMeetups),
+  takeLatest('@meetup/CREATE_MEETUP_REQUEST', createMeetup),
+]);
