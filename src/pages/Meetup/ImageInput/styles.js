207e5307095dd3a463a30f257f41e5f01394977e
Pjeferson <paulojeferson34@gmail.com>
2020-02-11 15:52:26 -0300

    Creating personalized image Input

---
+++ src/pages/Meetup/ImageInput/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+
+  height: 300px;
+  background: rgba(0, 0, 0, 0.4);
+
+  label {
+    cursor: pointer;
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    img {
+      width: 940px;
+      height: 300px;
+
+      object-fit: cover;
+      border-radius: 4px;
+    }
+
+    div {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      z-index: 2;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      color: #fff;
+      font-size: 20px;
+
+      opacity: 0.3;
+    }
+
+    input {
+      display: none;
+    }
+  }
+`;
