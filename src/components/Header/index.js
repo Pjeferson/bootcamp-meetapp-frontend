8bb614b3a5c0135f1cfef718b5cfa72d018bc8d1
Pjeferson <paulojeferson34@gmail.com>
2020-02-12 22:11:16 -0300

    Using logout action on it Header button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
+import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
   const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -17,7 +25,9 @@ export default function Header() {
             <strong>{profile.name}</strong>
             <Link to="/profile">Meu perfil</Link>
           </Profile>
-          <button type="button">Sair</button>
+          <button type="button" onClick={handleLogout}>
+            Sair
+          </button>
         </aside>
       </Content>
     </Container>
