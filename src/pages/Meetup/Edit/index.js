1f26ad94c5b7c535e8d27ea747aec5f454f72720
Pjeferson <paulojeferson34@gmail.com>
2020-02-14 18:25:29 -0300

    Validation Meetup edition form

--- src/pages/Meetup/Edit/index.js
+++ src/pages/Meetup/Edit/index.js
@@ -3,10 +3,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input, Textarea } from '@rocketseat/unform';
 import { MdAddCircleOutline } from 'react-icons/md';
 import PropTypes from 'prop-types';
+import * as Yup from 'yup';
 
 import { Container } from './styles';
 import ImageInput from '../ImageInput';
 import { editMeetupRequest } from '~/store/modules/meetup/actions';
+
+const schema = Yup.object().shape({
+  file_id: Yup.number().required('Selecione um banner para seu Meetup'),
+  title: Yup.string().required('Dê um título ao seu Meetup'),
+  description: Yup.string().required('Descreva o seu Meetup'),
+  date: Yup.string().required('Defina uma data para o Meetup'),
+  location: Yup.string().required('Defina o endereço do Meetup'),
+});
 
 export default function MeetupEdit({ match }) {
   const dispatch = useDispatch();
@@ -21,7 +30,7 @@ export default function MeetupEdit({ match }) {
 
   return (
     <Container>
-      <Form initialData={meetup} onSubmit={handleSubmit}>
+      <Form initialData={meetup} schema={schema} onSubmit={handleSubmit}>
         <ImageInput name="file_id" />
         <Input name="title" type="text" placeholder="Título do Meetup" />
         <Textarea name="description" placeholder="Descrição completa" />
